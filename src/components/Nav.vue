b8f59832663035c8eeb9ba840b9653c1ba4222a6
kmaurice012 <kingmaurice012@gmail.com>
2022-02-14 10:01:10 +0300

    first commit

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,95 +1,146 @@
-<template lang="html">
 
-  <div id="parentx">
+<template>
+  <div class="hidden">
+    <vs-sidebar absolute hover-expand reduce v-model="active" open>
+      <template #logo>
+        <!-- ...img logo -->
+      </template>
+      <vs-sidebar-item id="home">
+        <template #icon>
+          <i class="bx bx-home"></i>
+        </template>
+        Home
+      </vs-sidebar-item>
+      <vs-sidebar-item id="market">
+        <template #icon>
+          <i class="bx bx-grid-alt"></i>
+        </template>
+        Market Overview
+      </vs-sidebar-item>
+      <vs-sidebar-item id="Music">
+        <template #icon>
+          <i class="bx bxs-music"></i>
+        </template>
+        Music
+      </vs-sidebar-item>
+      <vs-sidebar-group>
+        <template #header>
+          <vs-sidebar-item arrow>
+            <template #icon>
+              <i class="bx bx-group"></i>
+            </template>
+            Social media
+          </vs-sidebar-item>
+        </template>
 
-    <vs-button @click="active=!active, notExpand = false" color="success" type="filled">Open Sidebar Reduce-expand</vs-button>
-    <vs-button @click="active=!active, notExpand = true" color="success" type="filled">Open Sidebar Reduce-only</vs-button>
-    <vs-sidebar :reduce="reduce" :reduce-not-hover-expand="notExpand" parent="body" default-index="1"  color="success" class="sidebarx" spacer v-model="active">
-
-      <div class="header-sidebar" slot="header">
-        <vs-avatar  size="70px" src="https://randomuser.me/api/portraits/men/85.jpg"/>
-      </div>
-      <vs-sidebar-group open title="Application">
-        <vs-sidebar-item index="1" icon="menu" @click="reduce=!reduce">
-         Toggle Sidebar
+        <vs-sidebar-item id="Instagram">
+          <template #icon>
+            <i class="bx bxl-instagram"></i>
+          </template>
+          Instagram
         </vs-sidebar-item>
-        <vs-sidebar-item index="5" icon="verified_user">
-          Configurations
+        <vs-sidebar-item id="twitter">
+          <template #icon>
+            <i class="bx bxl-twitter"></i>
+          </template>
+          Twitter
         </vs-sidebar-item>
-        <vs-sidebar-group title="Store">
-          <vs-sidebar-item index="2.1" icon="store">
-            Store
-          </vs-sidebar-item>
-          <vs-sidebar-item index="2.2" icon="nature_people">
-            Nature
-          </vs-sidebar-item>
-          <vs-sidebar-item index="2.3" icon="style">
-            Style
-          </vs-sidebar-item>
-        </vs-sidebar-group>
-        <vs-sidebar-item index="2" icon="gavel">
-          History
-        </vs-sidebar-item>
-        <vs-sidebar-item index="3" icon="https">
-          Security
-        </vs-sidebar-item>
-        <vs-sidebar-item index="4" icon="help">
-          Help
+        <vs-sidebar-item id="Facebook">
+          <template #icon>
+            <i class="bx bxl-facebook"></i>
+          </template>
+          Facebook
         </vs-sidebar-item>
       </vs-sidebar-group>
+      <vs-sidebar-group>
+        <template #header>
+          <vs-sidebar-item arrow>
+            <template #icon>
+              <i class="bx bx-code-alt"></i>
+            </template>
+            Coding
+          </vs-sidebar-item>
+        </template>
 
-
-      <vs-divider icon="person" position="left">
-        User
-      </vs-divider>
-
-
-      <vs-sidebar-item index="6" icon="account_box">
-        Profile
+        <vs-sidebar-item id="github">
+          <template #icon>
+            <i class="bx bxl-github"></i>
+          </template>
+          Github
+        </vs-sidebar-item>
+        <vs-sidebar-item id="codepen">
+          <template #icon>
+            <i class="bx bxl-codepen"></i>
+          </template>
+          Codepen
+        </vs-sidebar-item>
+        <vs-sidebar-item id="discord">
+          <template #icon>
+            <i class="bx bxl-discord"></i>
+          </template>
+          Discord
+        </vs-sidebar-item>
+        <vs-sidebar-item id="Javascript">
+          <template #icon>
+            <i class="bx bxl-javascript"></i>
+          </template>
+          Javascript
+        </vs-sidebar-item>
+        <vs-sidebar-item id="git">
+          <template #icon>
+            <i class="bx bxl-git"></i>
+          </template>
+          Git
+        </vs-sidebar-item>
+      </vs-sidebar-group>
+      <vs-sidebar-item id="donate">
+        <template #icon>
+          <i class="bx bxs-donate-heart"></i>
+        </template>
+        Donate
       </vs-sidebar-item>
+      <vs-sidebar-item id="drink">
+        <template #icon>
+          <i class="bx bx-drink"></i>
+        </template>
+        Drink
+      </vs-sidebar-item>
+      <vs-sidebar-item id="shopping">
+        <template #icon>
+          <i class="bx bxs-shopping-bags"></i>
+        </template>
+        Shopping
+      </vs-sidebar-item>
+      <vs-sidebar-item id="chat">
+        <template #icon>
+          <i class="bx bx-chat"></i>
+        </template>
+        Chat
+      </vs-sidebar-item>
+      <template #footer>
+        <vs-row justify="space-between">
+          <vs-avatar badge-color="danger" badge-position="top-right">
+            <i class="bx bx-bell"></i>
 
-      <div class="footer-sidebar" slot="footer">
-        <vs-button icon="settings" color="primary" type="border"></vs-button>
-      </div>
+            <template #badge> 28 </template>
+          </vs-avatar>
 
+          <vs-avatar>
+            <img src="/avatars/avatar-5.png" alt="" />
+          </vs-avatar>
+        </vs-row>
+      </template>
     </vs-sidebar>
   </div>
-
 </template>
-
 <script>
 export default {
-     name: "nav",
+  name:'nav',
   props: {},
-  data:()=>({
-    active:false,
-    notExpand: false,
-    reduce: true
-  })
-}
+  data: () => ({
+    active: "home",
+  }),
+};
 </script>
 
-<style lang="stylus">
-.header-sidebar
-  display flex
-  align-items center
-  justify-content center
-  flex-direction column
-  width 100%
-  h4
-    display flex
-    align-items center
-    justify-content center
-    width 100%
-    > button
-      margin-left 10px
-.footer-sidebar
-  display flex
-  align-items center
-  justify-content space-between
-  width 100%
-  > button
-      border 0px solid rgba(0,0,0,0) !important
-      border-left 1px solid rgba(0,0,0,.07) !important
-      border-radius 0px !important
-</style>
+        
